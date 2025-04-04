3fc8f04ba8676e639bfeb365ef358d30180369e6
Surapat Wongsri <khimnk635@gmail.com>
2025-04-05 03:41:58 +0700

    resendEmail

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,11 +1,34 @@
 import { auth } from "@/auth";
 import Header from "@/components/header";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
 import { ReactNode } from "react";
 
 const layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
   if (!session) redirect("/sign-in");
+
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    // get the user and see if the last activity date is  today
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session?.user?.id))
+      .limit(1);
+
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
+      return;
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id));
+  });
   return (
     <main className="flex min-h-screen flex-1 flex-col bg-dark-100 px-5 xs:px-10 md:px-16">
       <div className="mx-auto w-full max-w-7xl">
--- src/lib/workflow.ts
+++ src/lib/workflow.ts
@@ -1,3 +1,4 @@
+import { Client as QstashClient, resend } from "@upstash/qstash";
 import { Client as WorkflowClient } from "@upstash/workflow";
 import config from "./config";
 
@@ -5,3 +6,30 @@ export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+
+const qstashClient = new QstashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async ({
+  message,
+  subject,
+  email,
+}: {
+  email: string;
+  message: string;
+  subject: string;
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "Wakim <surapat.me>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+};
