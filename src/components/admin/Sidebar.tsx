96f5b59a9d432bc4ac45d8fca1c63f0b10016bb0
Surapat Wongsri <khimnk635@gmail.com>
2025-04-06 02:56:36 +0700

    sidebar

--- src/components/admin/Sidebar.tsx
+++ src/components/admin/Sidebar.tsx
@@ -1,15 +1,22 @@
 "use client";
 import { adminSideBarLinks } from "@/constants";
-import { getInitials } from "@/lib/utils";
+import { cn, getInitials } from "@/lib/utils";
+import { Book } from "lucide-react";
 import { Session } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import { Avatar, AvatarFallback } from "../ui/avatar";
-import { Button } from "../ui/button";
-import { ScrollArea } from "../ui/scroll-area";
 import { Separator } from "../ui/separator";
+// Import shadcn navigation menu components
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 
 const Sidebar = ({ session }: { session?: Session | null }) => {
   const pathname = usePathname();
@@ -24,46 +31,54 @@ const Sidebar = ({ session }: { session?: Session | null }) => {
     mounted && session?.user?.name ? getInitials(session.user.name) : "";
 
   return (
-    <div className="sticky left-0 top-0 flex h-dvh flex-col justify-between bg-base-100 border-r">
-      <ScrollArea className="flex-grow px-5 pb-5 pt-10">
-        <div className="flex flex-row items-center gap-2 border-b  pb-10 max-md:justify-center">
-          <Image
-            src="/icons/admin/logo.svg"
-            alt="logo"
-            width={37}
-            height={37}
-          />
-          <h1 className="text-lg font-bold md:block text-blue-950 hidden">
+    <div className="sticky left-0 top-0 flex h-dvh flex-col justify-between bg-base-100 border-r bg-muted/40">
+      <div className="flex-grow">
+        <div className="flex flex-row items-center gap-2 border-b p-5 pb-6 max-md:justify-center">
+          <Book size={37} className=" bg-primary text-white p-2 rounded-md" />
+          <h1 className="text-lg font-semibold md:block text-primary hidden">
             Admin Panel
           </h1>
         </div>
-        <div className="mt-10 flex flex-col gap-2">
-          {adminSideBarLinks.map((link) => {
-            const isSelected =
-              (link.route !== "/admin" &&
-                pathname.includes(link.route) &&
-                link.route.length > 1) ||
-              pathname === link.route;
-            return (
-              <Link href={link.route} key={link.route}>
-                <Button
-                  variant={isSelected ? "default" : "ghost"}
-                  className="w-full justify-start"
-                >
-                  <Image
-                    src={link.img}
-                    alt="icon"
-                    width={20}
-                    height={20}
-                    className={isSelected ? "brightness-0 invert" : ""}
-                  />
-                  <span className="ml-2 md:inline hidden">{link.text}</span>
-                </Button>
-              </Link>
-            );
-          })}
-        </div>
-      </ScrollArea>
+
+        <NavigationMenu className="mx-auto max-w-full mt-4">
+          <NavigationMenuList className="flex flex-col items-start gap-2 px-2 w-full">
+            {adminSideBarLinks.map((link) => {
+              const isSelected =
+                (link.route !== "/admin" &&
+                  pathname.includes(link.route) &&
+                  link.route.length > 1) ||
+                pathname === link.route;
+              return (
+                <NavigationMenuItem key={link.route} className="w-full">
+                  <Link href={link.route} legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={cn(
+                        navigationMenuTriggerStyle(),
+                        "w-full justify-start",
+                        isSelected
+                          ? "bg-primary text-primary-foreground"
+                          : "bg-transparent"
+                      )}
+                    >
+                      <Image
+                        src={link.img}
+                        alt="icon"
+                        width={20}
+                        height={20}
+                        className={isSelected ? "brightness-0 invert" : ""}
+                      />
+                      <span className="ml-2 md:inline hidden font-light">
+                        {link.text}
+                      </span>
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              );
+            })}
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+
       <Separator className="my-2" />
       <div className="flex items-center gap-4 px-5 pb-5">
         <Avatar>
